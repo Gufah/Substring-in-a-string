8748b2495b7af44d8612bdf83ef3c953815892b8
Ugonna Fredrick <ugonna.fredrick@outlook.com>
2021-04-20 13:31:14 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace SubstringInString
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string hayStack = "abcdefdgabcdefgabc";
+            string middle = "abc";
+
+            for (int i = 0; i < hayStack.Length - middle.Length + 1; i++)
+            {
+                if (hayStack.Substring(i, middle.Length).Equals(middle))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+
+    }
+}
